8d8a9ae3afab73ef415e17e0214ad4af0a48ff6f
c1042421 <c1042421@G-D103-20.local.thomasmore.be>
2018-04-18 09:26:24 +0200

    first commit

---
+++ src/java/hb5/it/www/Dummy.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hb5.it.www;
+
+/**
+ *
+ * @author c1042421
+ */
+public class Dummy {
+    
+}
