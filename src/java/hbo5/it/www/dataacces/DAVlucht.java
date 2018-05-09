4009b2159f77ca05791b639d512767f50deb1c04
c1042486 <c1042486@G-D103-19.local.thomasmore.be>
2018-05-09 11:35:58 +0200

    vordering

--- src/java/hbo5/it/www/dataacces/DAVlucht.java
+++ src/java/hbo5/it/www/dataacces/DAVlucht.java
@@ -5,8 +5,10 @@
  */
 package hbo5.it.www.dataacces;
 import hbo5.it.www.beans.Vlucht;
+import hbo5.it.www.factory.VluchtFactory;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 /**
@@ -18,27 +20,22 @@ public class DAVlucht extends DABase {
     public DAVlucht(String url, String login, String password, String driver) throws ClassNotFoundException {
         super(url, login, password, driver);
     }
-    public Vlucht getVlucht() {
+    public Vlucht getVlucht(int id) {
         Vlucht vlucht = null;
         
         try (
-            Connection connection = DriverManager.getConnection(url, login, password);
-             Statement statement = connection.createStatement();
-             ResultSet resultset = statement.executeQuery("SELECT * FROM vlucht where id < 5");)
-        {
-            if(resultset.next()){
-            vlucht = new Vlucht();
-            vlucht.setId(resultset.getInt("ID"));
-            vlucht.setVertrekluchthaven_id(resultset.getInt("vertrekluchthaven_id"));
-            vlucht.setAankomstluchthaven_id(resultset.getInt("aankomstluchthaven_id"));
-            vlucht.setVertrektijd(resultset.getDate("vertrektijd"));
-            vlucht.setAankomsttijd(resultset.getDate("aankomsttijd"));
-            vlucht.setCode(resultset.getString("code"));
-            }
-         } catch (Exception e) {
+                Connection connection = DriverManager.getConnection(url, login, password);
+                PreparedStatement statement = connection.prepareStatement("SELECT * FROM C1042421.VLUCHT WHERE vlucht_id =?");) {
+
+            statement.setInt(1, id);
+            ResultSet resultset = statement.executeQuery();
+            
+            return VluchtFactory.maakVluchtVanResultset(resultset);
+            
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return vlucht;
+        return null;
     }
             
         
