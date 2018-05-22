72056f31b822ade3aaec8ad6d82f9718b13dd628
c1042421 <c1042421@G-D103-20>
2018-05-22 14:09:07 +0200

    factory changes

--- src/java/hbo5/it/www/factory/VluchtBemanningFactory.java
+++ src/java/hbo5/it/www/factory/VluchtBemanningFactory.java
@@ -7,6 +7,7 @@ package hbo5.it.www.factory;
 import hbo5.it.www.beans.VluchtBemanning;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  *
@@ -15,16 +16,29 @@ import java.sql.SQLException;
 public class VluchtBemanningFactory {
     public static VluchtBemanning maakVluchtbemanningVanResultset(ResultSet resultset) throws SQLException{
         if (resultset.next()) {
-
-            VluchtBemanning vluchtbemanning = new VluchtBemanning();
-
-            vluchtbemanning.setId(resultset.getInt("ID"));
-            vluchtbemanning.setTaak(resultset.getString("TAAK"));
-            vluchtbemanning.setBemanningslid_id(resultset.getInt("BEMANNINGSLID_ID"));
-            vluchtbemanning.setVlucht_id(resultset.getInt("VLUCHT_ID"));
-
-            return vluchtbemanning;
+            return maakObject(resultset);
         }
         return null;
+    }
+    
+    public static VluchtBemanning maakObject(ResultSet resultset) throws SQLException {
+        VluchtBemanning vluchtbemanning = new VluchtBemanning();
+
+        vluchtbemanning.setId(resultset.getInt("ID"));
+        vluchtbemanning.setTaak(resultset.getString("TAAK"));
+        vluchtbemanning.setBemanningslid_id(resultset.getInt("BEMANNINGSLID_ID"));
+        vluchtbemanning.setVlucht_id(resultset.getInt("VLUCHT_ID"));
+
+        return vluchtbemanning;
+    }
+
+    public static ArrayList<VluchtBemanning> maakVluchtbemanningsLijstVanResultset(ResultSet resultset) throws SQLException {
+        ArrayList<VluchtBemanning> lijst = new ArrayList<>();
+        
+        while(resultset.next()) {
+            lijst.add(maakObject(resultset));
+        }
+        
+        return lijst;
     }
 }
